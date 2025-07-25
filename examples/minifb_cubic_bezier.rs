a85150522d5c1a4848b4c9eb8cd0a151f62abb55
Lucas Walter <wsacul@gmail.com>
2025-07-25 10:20:14 -0700

    drawing lines to show handles with animated bezier curve

--- examples/minifb_cubic_bezier.rs
+++ examples/minifb_cubic_bezier.rs
@@ -8,6 +8,58 @@ use minifb::{Key, Scale, ScaleMode, Window, WindowOptions};
 use noise::{NoiseFn, OpenSimplex};
 use stroke::f64::{CubicBezier, Point, PointN};
 
+fn put_pixel(argb: &mut [u32], x: f64, y: f64, width: usize, height: usize, r: u8, g: u8, b: u8) {
+    if x < 0.0 || y < 0.0 {
+        return;
+    }
+    let x = (x + 0.5) as usize;
+    let y = (y + 0.5) as usize;
+    if x >= width || y >= height {
+        return;
+    }
+    let pix_ind = y as usize * width + x as usize;
+    let a = 255;
+    argb[pix_ind] = a << 24 | (r as u32) << 16 | (g as u32) << 8 | b as u32;
+}
+
+fn get_line_pixels(x0: f64, y0: f64, x1: f64, y1: f64) -> Vec<(f64, f64)> {
+    let dy = y1 - y0;
+    let dx = x1 - x0;
+
+    let mut pixel_points = Vec::new();
+
+    let inc = 1;
+    let incf = inc as f64;
+
+    let (num_steps, x_step, y_step) = {
+        if dx.abs() > dy.abs() {
+            let slope = dy / dx.abs();
+            let num_steps = (dx.abs() as i32).max(1);
+            let x_step = incf * dx.signum();
+            let y_step = slope * incf;
+            (num_steps, x_step, y_step)
+        } else {
+            let slope = dx / dy.abs();
+            let num_steps = (dy.abs() as i32).max(1);
+            let x_step = slope * incf;
+            let y_step = incf * dy.signum();
+            (num_steps, x_step, y_step)
+        }
+    };
+
+    let mut x = x0;
+    let mut y = y0;
+    for _ in (0..num_steps).step_by(inc) {
+        pixel_points.push((x, y));
+        x += x_step;
+        y += y_step;
+    }
+
+    // println!("{x0:.2} {y0:.2} to {x1:.2} {y1:.2}");
+    // println!("{pixel_points:?}");
+    pixel_points
+}
+
 fn main() {
     let width = 320;
     let height = 180;
@@ -39,40 +91,41 @@ fn main() {
 
     let wd = width as f64;
     let ht = height as f64;
-    let handle_scale = wd * 0.7;
+    let handle_scale = wd * 0.6;
+    let scale = 0.8;
     let mut count = 0;
     while window.is_open() && !window.is_key_down(Key::Escape) {
         // end point
         let fr = count as f64 / 2000.0;
         let noise_pt = [fr, 0.0];
-        let x0 = x0_simplex.get(noise_pt) * wd + wd / 2.0;
+        let x0 = x0_simplex.get(noise_pt) * wd * scale + wd / 2.0;
         let noise_pt = [0.0, fr];
-        let y0 = y0_simplex.get(noise_pt) * ht + ht / 2.0;
+        let y0 = y0_simplex.get(noise_pt) * ht * scale + ht / 2.0;
         let c0 = PointN::new([x0, y0]);
 
         // handle
         let fr = count as f64 / 250.0;
         let noise_pt = [fr, 0.0];
-        let x1 = x1_simplex.get(noise_pt) * handle_scale;
+        let x1 = x0 + x1_simplex.get(noise_pt) * handle_scale;
         let noise_pt = [0.0, fr];
-        let y1 = y1_simplex.get(noise_pt) * handle_scale;
-        let h0 = PointN::new([x0 + x1, y0 + y1]);
+        let y1 = y0 + y1_simplex.get(noise_pt) * handle_scale;
+        let h0 = PointN::new([x1, y1]);
 
         // end point
         let fr = count as f64 / 2000.0;
         let noise_pt = [fr, 0.0];
-        let x3 = x3_simplex.get(noise_pt) * wd + wd / 2.0;
+        let x3 = x3_simplex.get(noise_pt) * wd * scale + wd / 2.0;
         let noise_pt = [0.0, fr];
-        let y3 = y3_simplex.get(noise_pt) * ht + ht / 2.0;
+        let y3 = y3_simplex.get(noise_pt) * ht * scale + ht / 2.0;
         let c3 = PointN::new([x3, y3]);
 
         // handle
         let fr = count as f64 / 250.0;
         let noise_pt = [fr, 0.0];
-        let x2 = x2_simplex.get(noise_pt) * handle_scale;
+        let x2 = x3 + x2_simplex.get(noise_pt) * handle_scale;
         let noise_pt = [0.0, fr];
-        let y2 = y2_simplex.get(noise_pt) * handle_scale;
-        let h1 = PointN::new([x3 + x2, y3 + y2]);
+        let y2 = y3 + y2_simplex.get(noise_pt) * handle_scale;
+        let h1 = PointN::new([x2, y2]);
 
         let bezier = CubicBezier::<PointN<2>, 2>::new(c0, h0, h1, c3);
 
@@ -87,24 +140,18 @@ fn main() {
             let x = p.axis(0);
             let y = p.axis(1);
 
-            let pix_ind = y as usize * width + x as usize;
-            if pix_ind >= 0 && pix_ind < (width * height) {
-                let a = 255;
-                let r = 255;
-                let g = 200;
-                let b = 180;
-                argb[pix_ind] = a << 24 | r << 16 | g << 8 | b;
+            if ind == 0 || ind == (nsteps - 1) {
+                // println!("{ind} {x:.2} {y:.2}");
             }
+
+            put_pixel(&mut argb, x, y, width, height, 255, 255, 128);
         }
 
-        for pt in [h0, h1] {
-            let pix_ind = pt.axis(0) as usize * width + pt.axis(1) as usize;
-            if pix_ind >= 0 && pix_ind < (width * height) {
-                let a = 255;
-                let r = 25;
-                let g = 250;
-                let b = 130;
-                argb[pix_ind] = a << 24 | r << 16 | g << 8 | b;
+        let handle_points0 = get_line_pixels(x0, y0, x1, y1);
+        let handle_points1 = get_line_pixels(x3, y3, x2, y2);
+        for handle_points in [handle_points0, handle_points1] {
+            for pt in handle_points {
+                put_pixel(&mut argb, pt.0, pt.1, width, height, 50, 250, 90);
             }
         }
 
